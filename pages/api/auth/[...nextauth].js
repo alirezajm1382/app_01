394006ade07127028b129f764a0777b62ac53a90
Alireza J <alirezajm@hotmail.com>
2022-12-23 19:35:08 +0330

    secret token added (not included in env file)

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,5 +8,6 @@ const authOptions = {
       clientSecret: "fad372930ad1cb00514c6c4f655ade417c59be7c",
     }),
   ],
+  secret: '9ec13b22bb173268ee1162e2a2ca9739'
 };
 export default NextAuth(authOptions);
